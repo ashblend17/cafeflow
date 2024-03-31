1f706818fbc3fa9350ffc6def49e58ba73cc79f6
Ansh <59763738+ashblend17@users.noreply.github.com>
2024-03-31 16:15:45 +0530

    init

---
+++ routes/user/orders.js
@@ -0,0 +1,178 @@
+const express = require('express');
+const router = express.Router();
+const max=100000;
+require('dotenv').config();
+const User=require('../../models/users');
+const checkAuth=require('../../middleware/auth');
+const products=require('../../models/products');
+//getting order details of user
+router.get('/',checkAuth, (req, res,next) => {
+    const userId = req.userData.userId;
+    User.findOne({userId}).exec()
+    .then(user=>{
+        if(!user){
+            return res.status(400).json({
+                message:'user not found'
+            })
+        }
+        else{
+            res.status(200).json({
+                message:'listed order details of user',
+                userId:userId,
+                orderDetails:user.orderDetails
+            })
+        }
+    })
+    .catch(err=>{
+        return res.status(500).json({
+            message:'Internal server error'
+        })
+    });
+});
+
+router.post('/',checkAuth, async (req, res, next) => {
+    const userId = req.userData.userId;
+    const { date, itemName,quantity} = req.body;
+
+    try {
+        const user = await User.findOne({ userId }).exec();
+        if (!user) {
+            return res.status(400).json({
+                message: 'User not found'
+            });
+        }
+        const product = await products.findOne({ itemName: itemName }).exec();
+        if (!product) {
+            return res.status(400).json({
+                message: 'Product is not available'
+            });
+        }
+        const status = 'pending';
+        const token = (User.lastToken || 0) + 1;
+        let totalAmount=0;
+        if (product.category === 'plastic') {
+            const plasticItem = {
+                date: date,
+                itemName: itemName,
+                token: token,
+                quantity: quantity,
+                status: status
+            };
+        user.plasticDetails.push(plasticItem);
+        } 
+        console.log("here");
+        totalAmount=product.cost*quantity;
+        const orderDetails = {
+            date: date,
+            itemName: itemName,
+            quantity: quantity,
+            totalAmount: product.cost * quantity,
+            status: status,
+            token: token
+        };
+        user.orderDetails.push(orderDetails);
+
+
+        
+        await user.save();
+
+        
+        return res.status(200).json({
+            message: 'Order placed successfully',
+            totalAmount: totalAmount,
+            token: token
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+});
+
+//confirming order by user
+//took orderid
+router.put('/confirm',checkAuth, async (req, res, next) => { 
+    const userId = req.userData.userId;
+    const { orderId } = req.body;
+    
+    try {
+        const user = await User.findOne({ userId }).exec();
+        if (!user) {
+            return res.status(400).json({
+                message: 'User not found'
+            });
+        }
+        const order = user.orderDetails.find(order => order._id.toString() === orderId);
+        if (!order) {
+            return res.status(400).json({ message: 'Order not found' });
+        }
+        order.status = 'confirmed';
+        await user.save();
+        return res.status(200).json({ message: 'Order confirmed successfully' });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+    };
+});
+
+// cancelling order
+router.put('/cancel',checkAuth,async (req,res,next)=>{
+    const userId = req.userData.userId;
+    const { orderId } = req.body;
+    
+    try {
+        const user = await User.findOne({ userId }).exec();
+        if (!user) {
+            return res.status(400).json({
+                message: 'User not found'
+            });
+        }
+        const order = user.orderDetails.find(order => order._id.toString() === orderId);
+        if (!order) {
+            return res.status(400).json({ message: 'Order not found' });
+        }
+        order.status = 'cancelled';
+        await user.save();
+        return res.status(200).json({ message: 'Order cancelled successfully' });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+    };
+});
+//payment
+router.put('/pay',checkAuth,async (req,res,next)=>{
+    const userId = req.userData.userId;
+    const { orderId } = req.body;
+    
+    try {
+        const user = await User.findOne({ userId }).exec();
+        if (!user) {
+            return res.status(400).json({
+                message: 'User not found'
+            });
+        }
+        const order = user.orderDetails.find(order => order._id.toString() === orderId);
+        if (!order) {
+            return res.status(400).json({ message: 'Order not found' });
+        }
+        order.status = 'paid';
+        await user.save();
+        return res.status(200).json({ message: 'payment successfull' });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: 'Internal server error'
+        });
+    };
+});
+module.exports = router;
+
---
+++ routes/user/plastics.js
@@ -0,0 +1,114 @@
+const express=require('express');
+const router=express.Router();
+require('dotenv').config();
+const checkAuth=require('../../middleware/auth');
+const User=require('../../models/users');
+router.get('/',checkAuth,(req,res,next)=>{
+    const userId = req.userData.userId;
+    User.findOne({userId}).exec()
+    .then(user=>{
+        if(!user){
+            return res.status(400).json({
+                message:'user not found'
+            })
+        }
+        else{
+            res.status(200).json({
+                message:'listed plastic items of user',
+                userId:userId,
+                plasticDetails:user.plasticDetails
+            })
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+        return res.status(400).json({
+            message:'Internal server error'
+        })
+    });
+});
+router.get('/plastic_count',checkAuth,(req,res,next)=>{
+    const userId = req.userData.userId;
+    User.findOne({userId}).exec()
+    .then(user=>{
+        if(!user){
+            return res.status(400).json({
+                message:'user not found'
+            })
+        }
+        else{
+            res.status(200).json({
+                message:'listed count of plastic items of user',
+                userId:userId,
+                plasticCount:user.plasticDetails.length
+            })
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+        return res.status(400).json({
+            message:'Internal server error'
+        })
+    });
+});
+
+router.post('/return',checkAuth,(req,res,next)=>{
+    const userId = req.userData.userId;
+    const {date,itemName,token,quantity}=req.body;
+    User.findOne( {userId})
+        .exec()
+    .then(user=>{
+        if(!user){
+            return res.status(400).json({
+                message:'user not found'
+            })
+        }
+        
+        else{
+            if(user.plasticDetails.length===0){
+                return res.status(400).json({
+                    message:'no palstics items to return'
+                })
+            }
+            else{
+                const plasticItem = user.plasticDetails.find(item => {
+                    return item.date === date && item.itemName === itemName && item.token === parseInt(token);
+                });
+
+                if (!plasticItem) {
+                    return res.status(400).json({
+                        message: 'Plastic item not found for the provided date, itemName, and token'
+                    });
+                }
+
+            if(plasticItem.quantity ===1){
+                user.plasticDetails.pull(plasticItem);
+            }
+            else if(plasticItem.quantity>1){
+                plasticItem.quantity-=quantity
+            }
+            
+            user.save()
+            .then(result=>{
+                res.status(200).json({
+                    message:'returned plastic item',
+                    userId:userId,
+                    plasticItem:plasticItem
+                })
+            })
+            .catch(err=>{
+                console.log(err);
+                res.status(500).json({
+                    message:'Internal server error'
+                })
+            });
+        }
+        }
+    })
+    .catch(err=>{
+        return res.status(500).json({
+            message:'Internal server error'
+        })
+    });
+});
+module.exports=router;
