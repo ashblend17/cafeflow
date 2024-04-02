d7054153e27d5a4d1f3c4c1c0a77ece7cd452283
idPriyanshu <146589021+idPriyanshu@users.noreply.github.com>
2024-04-02 15:05:13 +0530

    Delete frontend directory

--- frontend/android/app/src/main/kotlin/com/example/cafeflow/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cafeflow
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend/lib/environment.dart
+++
@@ -1,6 +0,0 @@
-class Environment {
-  static const Map<String, String> config = {
-    'IP': '172.16.198.221',
-    'port': '8000',
-  };
-}
