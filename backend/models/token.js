0fb772ee25ac3eaab02128cc2ec7185940eb6a58
Ansh <59763738+ashblend17@users.noreply.github.com>
2024-04-06 01:14:59 +0530

    Delete backend directory

--- backend/models/token.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const tokenSchema = new mongoose.Schema({
-    lastToken: { type: Number, required: true}
-});
-module.exports = mongoose.model('Token', tokenSchema);
